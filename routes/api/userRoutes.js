a269ba59df4704ecf9527c1f26e66b276b012489
Nancy Ramon <nramon513@gmail.com>
2024-11-28 06:05:10 -0600

    added thought and user routes to api

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,27 @@
+const router = require('express').Router();
+const {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
+} = require('../../controllers/userController');
+
+// Routes for users
+router.route('/')
+  .get(getAllUsers)
+  .post(createUser);
+
+router.route('/:id')
+  .get(getUserById)
+  .put(updateUser)
+  .delete(deleteUser);
+
+// Routes for friends
+router.route('/:userId/friends/:friendId')
+  .post(addFriend)
+  .delete(removeFriend);
+
+module.exports = router;
